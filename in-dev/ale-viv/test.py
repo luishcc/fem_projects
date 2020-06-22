3737adf7c73181e4f5b52cabfeeef13d305d8d84
luis <lh.carnevale@gmail.com>
2020-06-22 20:39:15 -0300

    Testing movement of fine mesh

--- in-dev/ale-viv/test.py
+++ in-dev/ale-viv/test.py
@@ -9,7 +9,7 @@ import semiLagrangean as sl
 
 cwd = os.getcwd()
 
-arquivo = "vivB"
+arquivo = "vivC"
 
 malha = Gm.GMesh("mesh/"+arquivo+".msh")
 x = malha.X
@@ -18,15 +18,11 @@ ien = malha.IEN
 nodes = len(x)
 num_ele = len(ien)
 
-dt = 0.0004
+dt = 0.001
 tempo = 200
-Re = 100
-v_in = Re
-psi_top = v_in * 10
 
-p_lagrange = 0.0
 p_smooth = 0.7
-p_wave = 0.0
+
 
 
 # ---------------------------------------
@@ -45,10 +41,7 @@ def move_cylinder(_nodes, _y, _y_max, _f_0, _t, _dt):
 # ---------------------------------------
 
 Psi_old = sp.zeros(nodes)
-Wz_old = sp.zeros(nodes)
-Wz_dep = sp.zeros(nodes)
-vx = sp.zeros(nodes, dtype="float64") + v_in
-vy = sp.zeros(nodes, dtype="float64")
+
 
 # ---------------------------------------
 # Montagem de matrizes
@@ -58,10 +51,7 @@ Boundary = sp.zeros(nodes)
 lista = []
 
 cylinder = Gm.get_boundary_with_tag("mesh/"+arquivo+".msh", 5)
-center = 0
-len_cylinder = len(cylinder)
-for i in cylinder:
-    center += y[i] / len_cylinder
+
 
 for i in range(nodes):
     if y[i] == 0.0 :
@@ -83,11 +73,12 @@ sp.random.seed(1)
 # -------------------------------------------------------------
 # ---------------------- Loop No Tempo ------------------------
 
+print("Neighbour Elements ")
 neighbour_ele, neighbour_nodes = sl.neighbourElements2(nodes, ien)
-for i in range(100):
-    vx_smooth, vy_smooth = Gm.weighted_smoothMesh(neighbour_nodes, Boundary, x, y, dt)
-    x = x + p_smooth*vx_smooth  * dt
-    y = y + p_smooth*vy_smooth * dt
+# for i in range(1):
+#     vx_smooth, vy_smooth = Gm.weighted_smoothMesh(neighbour_nodes, Boundary, x, y, dt)
+#     x = x + p_smooth*vx_smooth  * dt
+#     y = y + p_smooth*vy_smooth * dt
 
 for t in range(0, tempo-1):
     print("Solving System " + str((float(t)/(tempo-1))*100) + "%")
@@ -95,46 +86,25 @@ for t in range(0, tempo-1):
     y = move_cylinder(cylinder, y, 0.3, 16, t*dt, dt)
 
 
-    #vx_smooth = sp.zeros(nodes)
-    #vy_smooth = sp.zeros(nodes)
-    # for j in range(1):
-    #     vx_temp, vy_temp, x, y = Gm.smoothMesh_v2(neighbour_nodes, Boundary,
-    #                                               x, y, dt)
-    #     vx_smooth += vx_temp
-    #     vy_smooth += vy_temp
-
     vx_smooth, vy_smooth = Gm.weighted_smoothMesh(neighbour_nodes, Boundary, x, y, dt)
 
-    vx_wave = sp.random.rand(nodes) * 0.2*sp.cos(2*sp.pi*5*t*dt)
-    vy_wave = sp.random.rand(nodes) * 0.2*sp.cos(2*sp.pi*5*t*dt)
 
-    vxAle = p_lagrange * vx + p_smooth * vx_smooth + p_wave * vx_wave
-    vyAle = p_lagrange * vy + p_smooth * vy_smooth + p_wave * vy_wave
+    vxAle = p_smooth * vx_smooth
+    vyAle = p_smooth * vy_smooth
 
 
     for i in range(num_bc):
         index = int(Boundary[i])
         vxAle[index] = 0
         vyAle[index] = 0
-        if x[index] == 0:
-            vx[index] = v_in
-        if x[index] != 32.5:
-            vy[index] = 0.0
-            if index in cylinder:
-                vx[index] = 0
 
-    vx_sl = vx - vxAle
-    vy_sl = vy - vyAle
-
-    # x = x + (vxAle - vx_smooth) * dt
-    # y = y + (vyAle - vy_smooth) * dt
 
     x = x + vxAle  * dt
     y = y + vyAle * dt
 
     # Salvar VTK
-    vtk = Io.InOut(x, y, ien, len(x), len(ien), Psi_old, Wz_old, Wz_dep,
-                    None, None, vx, vy, vx_sl, vy_sl)
+    vtk = Io.InOut(x, y, ien, len(x), len(ien), Psi_old, Psi_old, Psi_old,
+                    None, None, Psi_old, Psi_old, Psi_old, Psi_old)
     vtk.saveVTK(cwd+"/results", "move" + str(t+1))
 
 
